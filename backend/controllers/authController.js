96b8c10ef1447cfc9370cdf4cbfda3c92f2f59b6
vivek prajapati <vivek011prajapati@gmail.com>
2026-01-01 16:08:27 +0530

    add config for JWT_SECRET

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -38,3 +38,38 @@ export const signUp = async (req, res) => {
          return res.status(500).json(`sigup error${error}`)
     }
 }
+
+export const signIn = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const user = await user.findOne({email})
+            if(!User){
+                return res.status(404).json({message:"user doesn`t exist"}) 
+            }
+        const isMatch = await bycrypt.compare(password, user.password)
+        if(!isMatch){
+             res.status(400).json({message:"password incorrect"})
+        }
+       
+        const hashpassword = await bycrypt.hash(password, 10)
+        user = await User.create({
+            fullName,
+            email,
+            mobileNumber,
+            role,
+            password:hashpassword
+        })
+        const token = await genToken(user._id)
+        res.cookie("token",token,{
+            secure:false,
+            sameSite:"strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly:true
+        })
+        return res.status(201).json(user)
+        
+
+    } catch (error) {
+         return res.status(500).json(`sigup error${error}`)
+    }
+}
