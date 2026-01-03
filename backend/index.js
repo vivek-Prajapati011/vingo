331c870f4756cdc25d368db883f4d43107ceb0b7
vivek prajapati <vivek011prajapati@gmail.com>
2026-01-03 21:05:57 +0530

    refactor: reorganize middleware setup and fix cors configuration

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import cookieParser from "cookie-parser"
+import authRoute from "./routers/authRoute.js"
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 5000
+app.use(cors({
+    Credential:true,
+    origin:"htpp://localhost5173"
+})) 
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth", authRoute)
 app.listen(port, () =>{
      connectDB()
     console.log(`server is running on ${port}`)
