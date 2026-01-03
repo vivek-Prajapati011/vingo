07d0892f5e7b993424d4c0605416a07956e61034
vivek prajapati <vivek011prajapati@gmail.com>
2026-01-03 21:24:19 +0530

    feat: add routes for OTP verification and password reset in authRoute

--- backend/routers/authRoute.js
+++ backend/routers/authRoute.js
@@ -1,9 +1,13 @@
 import express, { Router } from "express"
-import { signIn, signOUT, signUp } from "../controllers/authController"
+import { resetPassword, sendOtp, signIn, signOUT, signUp, verifyOtp } from "../controllers/authController"
 const authRoute = express.Router()
 
 authRoute.post("/signUp", signUp)
 authRoute.post("/signIn", signIn)
 authRoute.get("/signout", signOUT)
+authRoute.post("/send-otp", sendOtp)
+authRoute.post("/verify-otp", verifyOtp)
+authRoute.post("/reset-password", resetPassword)
+
 
 export default authRoute
